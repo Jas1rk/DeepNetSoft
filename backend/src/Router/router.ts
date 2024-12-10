fd62f4b6ba0e03589b38c8223f4ef910bed2f9d2
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-12-10 20:31:31 +0530

    making public

--- backend/src/Router/router.ts
+++ backend/src/Router/router.ts
@@ -5,9 +5,6 @@ const route = Router()
 
 route.post('/create-menu', createMenu);
 route.get('/create-menu',getMenu)
-route.get('/',(req,res)=>{
-    console.log("The Damsn")
-    res.json("Api working successfull")
-})
+
 
 export default route
